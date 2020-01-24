3b5ae5621a0d43afbf2cf670f3366d75eff1c6cc
unknown <kmh950320@gmail.com>
2020-01-24 03:11:49 +0100

    week1 edit code 2020-01-24

--- practice/week1.py
+++ practice/week1.py
@@ -4,7 +4,7 @@
 """
 작성자 : 김문희
 작성일 : 2020-01-16
-제목: 1주차 실습 
+제목: 1주차 실습
 """
 
 #실습 1
@@ -23,7 +23,7 @@ print(a - int(a)) #6
 print(0.0000000001234*9876)
 
 #실습 2
-import math 
+import math
 print(round(0.1 + 0.1 + 0.1, 2)) #1
 print(math.pi * 5.2**2)#2
 
@@ -32,18 +32,21 @@ print(math.pi * 5.2**2)#2
 print(((1000%99)%8)%5) #1
 print(4**4**4 + 3**3 + 2) # 2
 import fractions
-print(fractions.Fraction(5**-2/3**4)) # 3
-print(137%7)
+print(fractions.Fraction(3**4/5**(-2)) # 3
+print((1+137%7)%7)
 from math import pi
 print(int(pi*1000)/1000)
 
 #3. 변수
 
 #실습 1
+radius = 3.5 # 1번
+area = math.pi*radius**2
+print(area)
 
 money = 100
 rate = 0.25
-sum_1 = money*(1+rate)**12
+sum_1 = money*(1+rate)**12 #2번
 sum_2 = money*(1+rate)**24
 sum_3 = money*(1+rate)**36
 print(sum_1,sum_2,sum_3)
